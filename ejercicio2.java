a71841e7d8e2d984acd377346f9c6f0156f93bd0
Kenneth Gonzalez 201930442 <krgonzalopez@gmail.com>
2021-06-08 22:00:23 -0600

    se sube archivo .java ejercicio 2

---
+++ ejercicio2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ejercicio2{
+	
+	public static void main(String[] args){
+		String pais, capital;
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Ingrese el nombre del pais: ");
+		pais = scanner.nextLine();
+		System.out.print("Ingrese el nombre de la capital del pais: ");
+		capital = scanner.nextLine();
+		
+		System.out.println(capital+" es la capital de "+pais);
+		
+	}
+}
